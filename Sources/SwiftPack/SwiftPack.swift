cac2072af7285817e707c049f9a9d9f07738761a
0xLeif <zmeriksen@icloud.com>
2020-10-20 18:07:51 -0500

    Added @_exported

--- Sources/SwiftPack/SwiftPack.swift
+++ Sources/SwiftPack/SwiftPack.swift
@@ -4,8 +4,9 @@
 //  Created by Zach Eriksen on 10/20/20.
 //
 
-import UDLoader
-import Later
-import Object
-import EKit
-import E
+@_exported import FLite
+@_exported import UDLoader
+@_exported import Later
+@_exported import Object
+@_exported import EKit
+@_exported import E
