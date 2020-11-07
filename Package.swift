b35861e21c2264e27c7798e439c4582829d7be2e
Zach Eriksen <zmeriksen@icloud.com>
2020-11-07 12:51:28 -0600

    Switched to Swift 5.2 and removed platforms

--- Package.swift
+++ Package.swift
@@ -1,14 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftPack",
-    platforms: [
-        .iOS("13.0"),
-        .macOS("10.15")
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
